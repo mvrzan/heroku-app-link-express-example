75545015f2cb43c122834577c93b44bc49586836
mvrzan <mvrzan@icloud.com>
2025-03-26 16:31:45 -0400

    implement async getSegments controller with error handling and logging

--- server/src/controllers/getSegments.js
+++ server/src/controllers/getSegments.js
@@ -1,3 +1,15 @@
-const getSegments = () => {};
+const getSegments = async (req, res) => {
+  const { event, context, logger } = req.sdk;
+
+  try {
+    const accessToken = context?.org?.accessToken;
+    logger.info("accessToken", accessToken);
+
+    res.status(200).json({ message: "Route getSegments invocation was successful!", accessToken });
+  } catch (error) {
+    logger.error(error.message);
+    res.status(500).send(error);
+  }
+};
 
 export default getSegments;
