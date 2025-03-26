8afc238e739c9f6abc2d6719201f3019a7e28018
mvrzan <mvrzan@icloud.com>
2025-03-26 16:42:47 -0400

    implement localTestRoute with async handling and success response

--- server/src/controllers/localTestRoute.js
+++ server/src/controllers/localTestRoute.js
@@ -1,3 +1,7 @@
-const localTestRoute = () => {};
+const localTestRoute = async (req, res) => {
+  console.log("localTestRoute");
+
+  res.status(200).json({ message: "Route localTestRoute invocation was successful!" });
+};
 
 export default localTestRoute;
