3df3be36c6108d5c542f5d60e7efb10a699a7f29
mvrzan <mvrzan@icloud.com>
2025-03-27 16:14:24 -0400

    enhance healthcheck controller with logging for request and response details

--- server/src/controllers/healthcheck.js
+++ server/src/controllers/healthcheck.js
@@ -1,4 +1,10 @@
-const healthcheck = (_req, res) => {
+import { getCurrentTimestamp } from "../utils/loggingUtil.js";
+
+const healthcheck = (req, res) => {
+  console.log(`${getCurrentTimestamp()} 🏥 - healthcheck - Healthcheck request received!`);
+  console.log("request", req);
+  console.log("response", res);
+
   res.status(200).send("OK");
 };
 
