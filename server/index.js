0f2166c72b1196e486fb01cfab573baa61da17a1
mvrzan <mvrzan@icloud.com>
2025-03-27 19:14:20 -0400

    fix: remove await from salesforceRoutes middleware usage

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors(corsOptions));
 app.use(noneSalesforceRoutes);
-app.use(await salesforceRoutes);
+app.use(salesforceRoutes);
 
 app.listen(port, () => {
   console.log(`${getCurrentTimestamp()} 🎬 Authentication server listening on port: ${port}`);
