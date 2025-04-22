eef9be3ede836c1bd5f89fa0fa126897b6653fe2
mvrzan <mvrzan@icloud.com>
2025-04-22 13:54:18 -0400

    add getJoke controller and route for fetching jokes

---
+++ server/src/controllers/joke/getJoke.js
@@ -0,0 +1,31 @@
+import JokeAPI from "sv443-joke-api";
+import { getCurrentTimestamp } from "../../utils/loggingUtil.js";
+
+const getJoke = async (_req, res) => {
+  try {
+    console.log(`${getCurrentTimestamp()} 🪬 - getJokes - Request received...`);
+
+    const response = await JokeAPI.getJokes();
+
+    console.log(response);
+    const data = await response.json();
+
+    const joke = `${data.setup} --- ${data.delivery}`;
+
+    if (!response.ok) {
+      throw new Error(`There was an error when trying to get a joke: ${response.status} - ${response.statusText}`);
+    }
+
+    console.log(`${getCurrentTimestamp()} ✅ - getJokes - A joke was successfully provided!`);
+
+    res.status(200).send({
+      message: "A funny joke!",
+      joke: joke,
+    });
+  } catch (error) {
+    console.error(`${getCurrentTimestamp()} ❌ - getJokes - Error occurred: ${error.message}`);
+    res.status(500).send(error);
+  }
+};
+
+export default getJoke;
--- server/src/routes/none-salesforce-routes.js
+++ server/src/routes/none-salesforce-routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import localTestRoute from "../controllers/localTestRoute.js";
+import getJoke from "../controllers/joke/getJoke.js";
 
 const noneSalesforceRoutes = Router();
 
 noneSalesforceRoutes.get("/v1/localTestRoute", localTestRoute);
+noneSalesforceRoutes.get("/v1/jokes", getJoke);
 
 export default noneSalesforceRoutes;
