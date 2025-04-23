e7dc84eaf507193b82b6796a3ab1d46b5a7fc949
mvrzan <mvrzan@icloud.com>
2025-04-23 09:27:18 -0400

    add getTodos controller for fetching todo items

---
+++ server/src/controllers/todo/getTodos.js
@@ -0,0 +1,30 @@
+import { getCurrentTimestamp } from "../../utils/loggingUtil.js";
+
+const getTodos = async (req, res) => {
+  try {
+    console.log(`${getCurrentTimestamp()} 🪬 - getTodos - Request received...`);
+
+    const userId = req.body?.userId ? 1 : req.body.userId;
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${userId}`);
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(
+        `There was an error when trying to get todo information: ${response.status} - ${response.statusText}`
+      );
+    }
+
+    console.log(`${getCurrentTimestamp()} ✅ - getTodos - Todos were successfully provided!`);
+
+    res.status(200).send({
+      message: "Todo items",
+      todos: data,
+    });
+  } catch (error) {
+    console.error(`${getCurrentTimestamp()} ❌ - getTodos - Error occurred: ${error.message}`);
+    res.status(500).send(error);
+  }
+};
+
+export default getTodos;
