e2ba6b564dac22144b68005ef8be5fa8b37064fc
mvrzan <mvrzan@icloud.com>
2025-04-23 09:29:03 -0400

    update todos routes to use consistent naming and add postTodos endpoint

--- server/src/routes/none-salesforce-routes.js
+++ server/src/routes/none-salesforce-routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import localTestRoute from "../controllers/localTestRoute.js";
 import getJoke from "../controllers/joke/getJoke.js";
 import getTodos from "../controllers/todo/getTodos.js";
+import postTodos from "../controllers/todo/postTodos.js";
 
 const noneSalesforceRoutes = Router();
 
 noneSalesforceRoutes.get("/v1/localTestRoute", localTestRoute);
 noneSalesforceRoutes.get("/v1/joke", getJoke);
-noneSalesforceRoutes.get("/v1/todos", getTodos);
+noneSalesforceRoutes.get("/v1/get-todos", getTodos);
+noneSalesforceRoutes.post("/v1/post-todos", postTodos);
 
 export default noneSalesforceRoutes;
